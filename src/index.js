0fae4bc060c401e662a7dfc2445a2bcca36085c6
Mariusz Wieleba <mariusz@getsling.com>
2021-08-11 09:41:00 +0200

    Minor fix

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-const fs = require("fs");
-const { createCanvas, loadImage } = require("canvas");
-const layers = require("./config");
+const fs = require('fs');
+const { createCanvas, loadImage } = require('canvas');
+const layers = require('./config');
 
 const canvas = createCanvas(500, 500);
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 const edition = 10;
 
-const saveLayer = (edition) => {
-  fs.writeFileSync(
-    `./src/output/${edition}set.png`,
-    canvas.toBuffer("image/png")
-  );
+const saveLayer = edition => {
+  fs.writeFileSync(`./src/output/${edition}set.png`, canvas.toBuffer('image/png'));
 };
 
 const drawLayer = async (layer, edition) => {
-  let element =
-    layer.elements[Math.floor(Math.random() * layer.elements.length)];
+  let element = layer.elements[Math.floor(Math.random() * layer.elements.length)];
   const image = await loadImage(`${layer.location}/${element.fileName}`);
   ctx.drawImage(image, 0, 0, 500, 500);
   saveLayer(edition);
 };
-const assets = [];
 
 for (let i = 1; i <= edition; i++) {
   layers.forEach((layer, index) => {
-    drawLayer(layer, i);
+    drawLayer(layer, index);
   });
-  console.log(i, "NFT item created! 🚀");
+  console.log(i, 'NFT item created! 🚀');
 }
 
 // for (let i = 1; i <= edition; i++) {
